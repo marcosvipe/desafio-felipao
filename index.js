dafe7fe7f9f269e70e4dac459dbbe11938b2f0e9
Marcos Vinicios <marcvpluck@gmail.com>
2025-04-28 17:02:30 -0300

    Estrutura de decisão para definir nível do Heroi

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("oi")
+// Variáveis
+let nome = "Sentinela Cósmico"
+let xp = 10000
+let nivel
+
+// Estrutura de decisão
+if (xp < 1000) {
+    nivel = "Fero"
+} else if (xp <= 2000) {
+    nivel = "Bronze"
+} else if (xp <= 5000) {
+    nivel = "Prata"
+} else if (xp <= 7000) {
+    nivel = "Ouro"
+} else if (xp <= 8000) {
+    nivel = "Platina"
+} else if (xp <= 9000) {
+    nivel = "Ascendente"
+} else if (xp <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
